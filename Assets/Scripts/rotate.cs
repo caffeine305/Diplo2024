82ac1ddd2b61e840750e7bb7541a4c9a594174d0
Ricardo Lázaro <caffeine305@gmail.com>
2024-10-29 16:22:09 -0600

    Changed floor texture. Added Flames.

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+
+	public float rotationVelocity;
+    public Vector3 rot = Vector3.zero;
+	
+	void Update () {
+        this.transform.Rotate(rot*Time.deltaTime * rotationVelocity);
+	}
+}
